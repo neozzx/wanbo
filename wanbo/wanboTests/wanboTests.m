d270d338df32e90c8f63049fae724099cac4e84f
zhangzhenxiao <neozzx@163.com>
2018-04-10 10:34:08 +0800

    upload

---
+++ wanbo/wanboTests/wanboTests.m
@@ -0,0 +1,39 @@
+//
+//  wanboTests.m
+//  wanboTests
+//
+//  Created by 震霄 张 on 2018/4/10.
+//  Copyright © 2018年 neo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wanboTests : XCTestCase
+
+@end
+
+@implementation wanboTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
